56b57e11d5c5191c8b4472d677d810862d670db6
Frederick Robinson <frederick.robinson@gmail.com>
2019-02-22 08:27:05 +0000

    refactor

--- main.go
+++ main.go
@@ -24,31 +24,44 @@ func main() {
 		{"e", "g"},
 	}
 
+	colorMap, solveable := solveGraph(edges, 4)
+	if !solveable {
+		panic("unsat")
+	}
+	fmt.Println(colorMap)
+}
+
+func solveGraph(edges [][2]string, nColors uint8) (map[string]uint8, bool) {
 	prob := smt.NewProb()
 	colorMap := map[string]*smt.UInt8{}
-	colorMax := prob.NewUInt8Const(4)
+	colorMax := prob.NewUInt8Const(nColors)
 
 	for _, e := range edges {
 		f, t := e[0], e[1]
 		if _, ok := colorMap[f]; !ok {
-			x := prob.NewUInt8()
-			prob.Assert(x.Lt(colorMax))
-			colorMap[f] = x
+			colorMap[f] = newColor(colorMax, prob)
 		}
 		if _, ok := colorMap[t]; !ok {
-			x := prob.NewUInt8()
-			prob.Assert(x.Lt(colorMax))
-			colorMap[t] = x
+			colorMap[t] = newColor(colorMax, prob)
 		}
 		prob.Assert(colorMap[f].Neq(colorMap[t]))
 	}
 
 	solveable := prob.Solve(solver.Solve)
 	if !solveable {
-		panic("unsat")
+		return nil, false
 	}
 
+	ans := map[string]uint8{}
 	for k, v := range colorMap {
-		fmt.Println(k, v.SolVal())
+		ans[k] = v.SolVal()
 	}
+
+	return ans, true
+}
+
+func newColor(colorMax *smt.UInt8, prob *smt.Problem) *smt.UInt8 {
+	x := prob.NewUInt8()
+	prob.Assert(x.Lt(colorMax))
+	return x
 }
